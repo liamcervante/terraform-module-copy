8093b8923d4ecb53ea5a24d69d90d812ce846b8f
Liam Cervante <liam.cervante@hashicorp.com>
2023-08-17 14:14:45 +0200

    make module structure match expected

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
-variable "id" {
-  type = string
+terraform {
+  required_providers {
+    tfcoremock = {
+      source = "hashicorp/tfcoremock"
+    }
+  }
 }
 
 data "tfcoremock_simple_resource" "external" {
-    id = var.id
+  id = var.id
 }
 
 resource "tfcoremock_simple_resource" "internal" {
-    string = data.tfcoremock_simple_resource.external.string
-    number = data.tfcoremock_simple_resource.external.number
-    boolean = data.tfcoremock_simple_resource.external.boolean
-}
-
-output "id" {
-    value = tfcoremock_simple_resource.internal.id
+  string  = data.tfcoremock_simple_resource.external.string
+  number  = data.tfcoremock_simple_resource.external.number
+  boolean = data.tfcoremock_simple_resource.external.boolean
 }
